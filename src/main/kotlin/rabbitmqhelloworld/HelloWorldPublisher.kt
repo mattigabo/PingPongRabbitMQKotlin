4013cb854656ec6f37f8a875d16030b2a2124d74
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-10-11 18:02:55 +0200

    CLEANED unnecessary code

--- src/main/kotlin/rabbitmqhelloworld/HelloWorldPublisher.kt
+++ src/main/kotlin/rabbitmqhelloworld/HelloWorldPublisher.kt
@@ -13,24 +13,15 @@ class HelloWorldPublisher {
         connection = factory.newConnection()
         channel = connection.createChannel()
 
-        // create a
-        // non-durable (the queue will not survive a broker restart)
-        // exclusive (used by only one connection and the queue will be deleted when that connection closes)
-        // autodelete (queue that has had at least one consumer is deleted when last consumer unsubscribes)
-        // queue with a generated name:
-        val queueName: String = channel.queueDeclare().queue
-
         val exchangeName = "HelloExchange"
         //create the exchange
         channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT)
 
-        //bind the queue to the exchange
-        //after this the queue will receive from the exchange all message with the specified routing key
         val routingKey = "helloMessage"
 
         val message: String = "Hello World RabbitMQ"
 
-        //Publish message to the exchange
+        //Publish message to the exchange with the defined routing key
         channel.basicPublish(exchangeName, routingKey, null, message.toByteArray(charset("UTF-8")))
         println(" [x] Sent '$message'")
 
